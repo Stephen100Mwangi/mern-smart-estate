5b84d47fde01762fe5eb137b24bc1a3b5f706e53
stephen100mwangi <mwangiwahome70@gmail.com>
2024-02-28 10:36:28 +0300

    Added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div>
-      <p className="text-red-500">Hello</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signIn' element={<SignIn />}/>
+        <Route path='/signUp' element={<SignUp />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
